dc3af20858431a4aed41f5c1778d9c9614415d6b
신창화 <sch9270@gmail.com>
2023-09-14 13:24:31 +0900

    Drill01 (2020182016신창화)
    과제 제출

---
+++ WASD.py
@@ -0,0 +1,29 @@
+import turtle
+import random
+
+def Move_Up():
+    turtle.stamp()
+    turtle.setheading(90)
+    turtle.forward(50)
+
+def Move_Left():
+    turtle.stamp()
+    turtle.setheading(180)
+    turtle.forward(50)
+def Move_Right():
+    turtle.stamp()
+    turtle.setheading(0)
+    turtle.forward(50)
+def Move_Down():
+    turtle.stamp()
+    turtle.setheading(-90)
+    turtle.forward(50)
+
+
+turtle.shape('turtle')
+
+turtle.onkey(Move_Up, 'w')
+turtle.onkey(Move_Left, 'a')
+turtle.onkey(Move_Right, 'd')
+turtle.onkey(Move_Down, 's')
+turtle.listen()
